1c8f9adb0af297ca821e327940765584fd2d6745
manaten <manaten@manaten.net>
2019-08-26 01:31:53 +0900

    feat(diff): リダイレクトページを判定し、別出力するようにした。

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const JA_BASE_URL = 'https://dont-starve.fandom.com/ja';
 const EN_BASE_URL = 'https://dontstarve.fandom.com';
 const WAIT_MSEC = 200;
 
-const wait = (msec) => new Promise(resolve => setTimeout(resolve, msec));
+const wait = (msec = WAIT_MSEC) => new Promise(resolve => setTimeout(resolve, msec));
 
 const listAllEntries = async (baseUrl) => {
   const result = [];
@@ -18,23 +18,52 @@ const listAllEntries = async (baseUrl) => {
     if (!offset) {
       return result;
     }
-    await wait(WAIT_MSEC);
+    await wait();
   }
+};
+
+const getEntryAsSimpleJson = async (id, baseUrl = EN_BASE_URL) => {
+  const { data } = await axios.get(`${baseUrl}/api/v1/Articles/AsSimpleJson?id=${id}`);
+  return data;
 };
 
 const main = async () => {
 
-  console.log(`ページ更新時刻: ${dateFormat(new Date(), "yyyy年mm月dd日 HH時MM分ss秒")}`);
+  console.log(`ページ更新時刻: '''${dateFormat(new Date(), "yyyy年mm月dd日 HH時MM分ss秒")}'''`);
   console.log('\n');
   
   const jaData = await listAllEntries(JA_BASE_URL);
-  console.log(`日本語版wikiには${jaData.length}のページがあります。`);
+  console.log(`更新日現在、日本語版wikiには'''${jaData.length}'''のページがあり、`);
   
   const enData = await listAllEntries(EN_BASE_URL);
-  console.log(`英語版wikiには${enData.length}のページがあります。`);
+  console.log(`英語版wikiには'''${enData.length}'''のページがあります。`);
   
   console.log('\n');
   const diff = lodash.differenceBy(enData, jaData, item => item.title);
-  console.log(diff.map(item => `* [[${item.title}]] ({{en|${item.title}|英語}})`).join('\n'));
+
+  const result = [];
+  for (const item of diff) {
+    const entryData = await getEntryAsSimpleJson(item.id);
+    
+    let isRedirect = false;
+    let redirectTo = '';
+
+    try {
+      isRedirect = /^redirect/i.test(entryData.sections[0].content[0].elements[0].text);
+      redirectTo = isRedirect ? entryData.sections[0].content[0].elements[0].text.replace(/^redirect\s*/i, '') : '';
+    } catch(e) {
+      console.error(e, item, JSON.stringify(entryData, null, 2));
+    }
+    result.push({ ...item, isRedirect, redirectTo });
+    
+    await wait();
+  }
+
+  console.log('==通常のページ==');
+  console.log(result.filter(i => !i.isRedirect).map(item => `* [[${item.title}]] ({{en|${item.title}|英語}})`).join('\n'));
+  
+  console.log('\n');
+  console.log('==リダイレクトページ==');
+  console.log(result.filter(i => i.isRedirect).map(item => `* [[${item.title}]] ({{en|${item.title}|英語}}) => [[${item.redirectTo}]] ({{en|${item.redirectTo}|英語}})`).join('\n'));
 };
 main().catch(e => console.error(e));
